662bbc4af64e03b9c805bd29de3c5fcf6f766e40
snaselj <snaselj@earth.snasel.net>
2015-12-29 22:11:19 +0100

    Follow: ORM

--- content.js
+++ content.js
@@ -20,17 +20,17 @@ exports = O.init();
 
 // Public {{{1
 exports.addModule('lib/index');
-exports.addModule('lib/item/html5/list');
+//exports.addModule('lib/item/html5/list');
 exports.addModule('lib/item/index');
-exports.addModule('lib/player/html5/common');
-exports.addModule('lib/player/html5/detail');
+//exports.addModule('lib/player/html5/common');
+//exports.addModule('lib/player/html5/detail');
 //exports.addModule('lib/player/gaia/side');
 exports.addModule('lib/player/commands');
 exports.addModule('lib/player/dvb');
 exports.addModule('lib/player/index');
 exports.addModule('lib/player/volume');
 exports.addModule('lib/player/playback');
-exports.addModule('lib/stream/html5/list');
+//exports.addModule('lib/stream/html5/list');
 exports.addModule('lib/stream/index');
 
 // }}}1
